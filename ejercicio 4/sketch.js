ec1866bf58f5fe794833a01162fd94da75525485
flashboy2324 <lucasvasconcellos01@gmail.com>
2025-09-08 15:16:36 -0300

    Ejercicio 4 Terminado
    Recordar usar Live Server en VSC

---
+++ ejercicio 4/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  select('body').style('background-color','yellow');
+  select('#uno').addClass('fondoazul');
+  select('#dos').addClass('fondorojo');
+  select('#tres').addClass('fondoverde');
+  select('#cuatro').addClass('fondovioleta');
+}
+
+function draw() {
+  
+}
